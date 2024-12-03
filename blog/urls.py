300bade9918cfe895dc6c77b63fe7b5afbf4aac8
Varad J <varad.gcs@gmail.com>
2024-12-04 02:52:07 +0530

    init landing page and dump it into base.html

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home, login
-
-urlpatterns = [
-    path('', home, name='home'),  # Home page URL
-    path('login/', login.as_view(), name='login'),  # Login page URL
-]
