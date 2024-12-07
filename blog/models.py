b30a9e4c46408dc724ec91348fd39321d3894190
Varad <varad.gcs@gmail.com>
2024-12-07 17:35:23 +0530

    init login form

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    thumbnail = models.ImageField(upload_to='thumbnails/')
+    title = models.TextField()
+    summary = models.TextField()
+    body = models.TextField()
+    last_updated = models.DateTimeField(auto_now=True)
