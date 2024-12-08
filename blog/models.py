bd3b476b42ef4fafb50f09f41b7662df47a545a5
Varad <varad.gcs@gmail.com>
2024-12-09 02:16:27 +0530

    Restructure templates

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
 class Blog(models.Model):
     thumbnail = models.ImageField(upload_to='thumbnails/')
     title = models.TextField()
     summary = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name='blogs')
     body = models.TextField()
+    body_html = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
