8869452a13eeb469db65ae9fd7a2f89ff0156377
Varad J <varad.gcs@gmail.com>
2024-12-09 19:14:46 +0530

    Set indian timezone

--- sportblog/settings.py
+++ sportblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n&41m&zi)!(@&f*v^)3&$8-y6g&gv#&y4(0lb4*t&nzer)4fv4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UCT'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
